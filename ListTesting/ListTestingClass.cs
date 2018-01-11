28075c9ca5f1b852fa7536ca850a8dac235c1836
Liam Houghton <liamhoughton90@yahoo.com>
2018-01-11 15:56:12 -0600

    Second commit, not much further

---
+++ ListTesting/ListTestingClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyListSolution;
+
+namespace ListTesting
+{
+    [TestClass]
+    public class ListTestingClass
+    {
+        [TestMethod]
+        public void AddIntCountTest()
+        {
+            //arrange
+            MyList<int> numberTest = new MyList<int>();
+            int numbers = 7;
+            //act
+            numberTest.AddingItem(numbers);
+            //assert
+            Assert.AreEqual(1, numberTest.Count);
+        }
+    }
+}
---
+++ MyListSolution/MyList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyListSolution
+{
+    public class MyList<T>
+    {
+        public void AddingItem(T item)
+        {
+
+        }
+
+        public void RemovingItem()
+        {
+
+        }
+
+        public void OverloadOperator()
+        {
+
+        }
+
+        public void NumberCount(T item)
+        {
+
+        }
+
+        public void ZipingTwoCustomClasses()
+        {
+
+        }
+        
+    }
+}
